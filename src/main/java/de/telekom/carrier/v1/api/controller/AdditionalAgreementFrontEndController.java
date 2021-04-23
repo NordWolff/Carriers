83011017c010eaa031d2136e4f0065b5e558cdc3
Thomas <51476041+NordWolff@users.noreply.github.com>
2021-04-23 20:05:32 +0200

    form add-additionalAgreement.html create with Controller Getmapping

--- src/main/java/de/telekom/carrier/v1/api/controller/AdditionalAgreementFrontEndController.java
+++ src/main/java/de/telekom/carrier/v1/api/controller/AdditionalAgreementFrontEndController.java
@@ -1,9 +1,12 @@
 package de.telekom.carrier.v1.api.controller;
 
+import de.telekom.carrier.v1.api.entity.AdditionalAgreement;
+import de.telekom.carrier.v1.api.entity.ServiceNumber;
 import de.telekom.carrier.v1.api.service.AdditionalAgreementService;
 import de.telekom.carrier.v1.api.service.CarrierService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,5 +25,12 @@ public class AdditionalAgreementFrontEndController {
         ModelAndView view = new ModelAndView("all-additionalAgreements");
         view.addObject("additionalAgreements", additionalAgreementService.findAll());
         return view;
+    }
+
+    @GetMapping(path = "/addAgreement")
+    public String showForm(Model model) {
+        model.addAttribute("additionalAgreement", new AdditionalAgreement());
+        model.addAttribute("carriers", carrierService.findAll());
+        return "add-additionalAgreement";
     }
 }
