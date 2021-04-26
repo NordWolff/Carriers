2ef128ad7f492894807f95e029d1ac19bac9acfa
A705848 <thomas.wolff@telekom.de>
2021-04-26 15:22:41 +0200

    CustomerAddressFrontEndController created and CustomerAddressRestController GET edit

--- src/main/java/de/telekom/carrier/v1/frontend/controller/CustomerAddressFrontEndController.java
+++ src/main/java/de/telekom/carrier/v1/frontend/controller/CustomerAddressFrontEndController.java
@@ -1,6 +1,5 @@
-package de.telekom.carrier.v1.api.controller;
+package de.telekom.carrier.v1.frontend.controller;
 
-import de.telekom.carrier.v1.api.entity.CustomerAddress;
 import de.telekom.carrier.v1.api.service.CarrierService;
 import de.telekom.carrier.v1.api.service.CustomerAddressService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,20 +8,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-
 @Controller
-@RequestMapping(path = "/api/v1/customerAddresses")
-public class CustomerAddressRestController {
-
-    @Autowired
-    CarrierService carrierService;
+@RequestMapping("/")
+public class CustomerAddressFrontEndController {
 
     @Autowired
     CustomerAddressService customerAddressService;
 
-    @GetMapping(path = "/findAll")
-    public List<CustomerAddress> findAll(){
-        return customerAddressService.findAll();
+    @Autowired
+    CarrierService carrierService;
+
+
+    @GetMapping(path = "/customerAddressesFindAll")
+    public ModelAndView findAll(){
+        ModelAndView view = new ModelAndView("all-customerAddresses");
+        view.addObject("addresses", customerAddressService.findAll());
+        return view;
     }
 }
